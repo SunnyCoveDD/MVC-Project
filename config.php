cf72659fc5853faabacf8c7a2c9a08cefa6a9fd3
SunnyCoveDD <dimakhov174@gmail.com>
2022-03-15 13:24:54 +0500

    Version 1.0.2

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_USERNAME = 'is342_5_Dimahov';
+const DB_PASSWORD = 'thH04IB53p';
+const DB_DATABASE = 'is342_5_MyDB';
 const DB_PORT = '3306'; # Default 3306
